2d0bd7821b70bed5cd649a2d936e18eed3793b09
diathanine <diathanine@gmail.com>
2020-11-25 18:23:11 +0800

    added file creation
    fleshed out new-list and add (item) began work on show

--- filework.py
+++ filework.py
@@ -1,7 +1,9 @@
 import click
+import os
+
 
 def get_path(name):
-    path="./"+name+"/"
+    target=path("storage/%s" %name)
 
 @click.command()
 def foo():
@@ -11,7 +13,15 @@ def foo():
 @click.argument('name')
 def new_list(name):
     click.echo('creating %s directory' %name)
-    
+    target_dir="storage/%s" %name
+    os.mkdir(target_dir)
+
+    os.chdir(target_dir)
+    click.echo(os.getcwd())
+    click.open_file('settings.txt', mode='w', encoding=None, errors='strict', lazy=False, atomic=False)
+    click.open_file('active.txt', mode='w', encoding=None, errors='strict', lazy=False, atomic=False)
+    click.open_file('complete.txt', mode='w', encoding=None, errors='strict', lazy=False, atomic=False)
+
 
 @click.command()
 @click.argument('name')
@@ -23,7 +33,9 @@ def delete_list(name):
 @click.argument('item')
 def add(name, item):
     click.echo('adding %s to %s' %(item, name))
-    #click.openfile()
+    target = 'storage/%s/active.txt' %name
+    active=click.open_file(target, mode='a', errors='strict', lazy=True, atomic=False)
+    active.write(item)
 
 @click.command()
 @click.argument('name')
@@ -38,9 +50,13 @@ def complete(name, item):
 def move(name, item, target):
     click.echo('moving %s from %s to %s' %(item, name, target))
 
+
 @click.command()
 @click.argument('name', default='.')
 def show(name):
+    target = 'storage/%s/active.txt' %name
+    list=click.open_file(target, mode='r')
+    click.echo(list)
     click.echo('%s' %name)
 
 @click.command()
